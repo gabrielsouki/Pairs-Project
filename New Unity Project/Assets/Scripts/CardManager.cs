3c930bb65dbca8860906fe92beef1f58fd534014
Gabriel Souki <gabrielsouki@gmail.com>
2021-10-09 16:48:44 -0400

    Se añadio el script CardManager, y dos objetos de prueba para las cartas

---
+++ New Unity Project/Assets/Scripts/CardManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public bool isSelected = false;
+    public CardManager par;
+
+    //Detectar clic
+    private void OnMouseDown()
+    {
+        Select();
+    }
+
+    public void Select()
+    {
+        if(isSelected == false)
+        {
+            isSelected = true;
+        }
+        else
+        {
+            isSelected = false;
+        }
+    }
+
+    private void Update()
+    {
+        Check();
+    }
+
+    public void Check()
+    {
+        if (isSelected && par.isSelected)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
