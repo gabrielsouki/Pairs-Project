4187fd58452ddbae4f29cf15a24667c18a89a519
Gabriel Souki <gabrielsouki@gmail.com>
2021-10-16 19:35:16 -0400

    Añadi el script con la clase Card

---
+++ New Unity Project/Assets/CardClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardClass : MonoBehaviour
+{
+    public class card
+    {
+        public string color;
+        public bool done;
+
+        public card(string m_color, bool m_done)
+        {
+            color = m_color;
+            done = m_done;
+        }
+    }
+
+    // Find the way to personalize each card class parameter
+
+    public card gameCard = new card("Red", false);
+}
