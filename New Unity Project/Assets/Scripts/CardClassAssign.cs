56fb83e3a79bb1147f34ed473e038822d8c6c544
Gabriel Souki <gabrielsouki@gmail.com>
2021-11-13 17:06:12 -0400

    Haciendo commit para ver si el error se soluciona

---
+++ New Unity Project/Assets/Scripts/CardClassAssign.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardClassAssign : MonoBehaviour
+{
+    public string m_color;
+    public bool done;
+
+    private void Start()
+    {
+        public CardClass.card card = new CardClass.card("Red", false);
+    }
+}
