75587d7360aa2dc6d34b20a2571873e4fd6fd59e
Gabriel Souki <gabrielsouki@gmail.com>
2021-11-13 21:14:12 -0400

    Mejorando CardClass y CardClassAssign

--- New Unity Project/Assets/Scripts/CardClassAssign.cs
+++ New Unity Project/Assets/Scripts/CardClassAssign.cs
@@ -1,11 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public class CardAssignment
+{
+    [SerializeField] string m_color;
+    [SerializeField] bool m_done;
+    [SerializeField] CardClass card;
+
+    public void Init()
+    {
+        card.Init(m_color, m_done);
+    }
+}
+
 public class CardClassAssign : MonoBehaviour
 {
-    public string m_color;
-    public bool done;
-    public CardClass.card newCard = new CardClass.card("Red", true);
-    
+    [SerializeField] List<CardAssignment> cards = new List<CardAssignment>();
+
+    private void Start()
+    {
+        /*foreach(var c in cards)
+        {
+            c.Init();
+        }*/
+
+        for (int i = 0; i < cards.Count; i++)
+        {
+            cards[i].Init();
+        }
+    }
 }
